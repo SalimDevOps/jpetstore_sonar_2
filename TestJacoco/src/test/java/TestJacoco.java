042b1b0b13d1635a95565221832e5532a9919f95
SalimDevOps <36883909+SalimDevOps@users.noreply.github.com>
2019-02-08 15:10:39 +0100

    Rename testJacoco.java to TestJacoco.java

--- TestJacoco/src/test/java/TestJacoco.java
+++ TestJacoco/src/test/java/TestJacoco.java
@@ -33,52 +33,52 @@ public  class testJacoco {
 		driver.findElement(By.cssSelector("#Catalog > form > input[type=\"submit\"]")).click();
 		// Cliquer sur la partie "Fish" 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(1) > img")).click();
-		// Sélectionner un produit 
+		// SÃ©lectionner un produit 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(2) > td:nth-child(1) > a")).click();	
-		// Ajout au panier l'item numéro 1 
+		// Ajout au panier l'item numÃ©ro 1 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(2) > td:nth-child(5) > a")).click();
 		// Cliquer sur "Return at the menu" 
 		driver.findElement(By.cssSelector("#BackLink > a")).click();
-		// Cliquer sur la séction "Dog" dans le menu gauche 
+		// Cliquer sur la sÃ©ction "Dog" dans le menu gauche 
 		driver.findElement(By.cssSelector("#SidebarContent > a:nth-child(4) > img")).click();
-		// Sélectionner le dernier produit 
+		// SÃ©lectionner le dernier produit 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(7) > td:nth-child(1) > a")).click();
 		// Cliquer sur le 2eme item ID 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(3) > td:nth-child(1) > a")).click();
 		// Cliquer sur "Add to Cart" 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(7) > td > a")).click();
-		// Sélectionner la séction "Réptiles" dans le menu de haut 
+		// SÃ©lectionner la sÃ©ction "RÃ©ptiles" dans le menu de haut 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(5) > img")).click();
-		// Sélectionner le premier produit 
+		// SÃ©lectionner le premier produit 
 		driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[1]/a")).click();
-		// Ajout au panier l'item numéro 1 
+		// Ajout au panier l'item numÃ©ro 1 
 		driver.findElement(By.xpath("//*[@id=\"Catalog\"]/table/tbody/tr[2]/td[5]/a")).click();
-		// Cliquer sur le logo pour retourner à la page d'accueil 
+		// Cliquer sur le logo pour retourner Ã  la page d'accueil 
 		driver.findElement(By.cssSelector("#LogoContent > a > img")).click();
 		// Cliquer sur l'iamge d'oiseau au millieu de la page 
 		driver.findElement(By.cssSelector("#MainImageContent > map > area:nth-child(1)")).click();
-		// Cliquer sur le logo pour retourner à la page d'accueil 
+		// Cliquer sur le logo pour retourner Ã  la page d'accueil 
 		driver.findElement(By.cssSelector("#LogoContent > a > img")).click();
 		// Ecrire "fish" sur la barre de recherche 
 		driver.findElement(By.cssSelector("#SearchContent > form > input[type=\"text\"]:nth-child(1)")).clear();
 		driver.findElement(By.cssSelector("#SearchContent > form > input[type=\"text\"]:nth-child(1)")).sendKeys("fish");
 		// Cliquer sur le bouton "Search" 
 		driver.findElement(By.cssSelector("#SearchContent > form > input[type=\"submit\"]:nth-child(2)")).click();
-		// Cliquer sur la première image 
+		// Cliquer sur la premiÃ¨re image 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(2) > td:nth-child(1) > a > img")).click();
 		// Ajout le premier produit dans le panier 
 		driver.findElement(By.cssSelector("#Catalog > table > tbody > tr:nth-child(3) > td:nth-child(5) > a")).click();
-		// Cliquer sur la séction "Cats" 
+		// Cliquer sur la sÃ©ction "Cats" 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(7) > img")).click();
 		// Cliquer sur le panier 
 		driver.findElement(By.cssSelector("#MenuContent > a:nth-child(1) > img")).click();
 		// Supprimer le premier produit 
 		driver.findElement(By.cssSelector("#Cart > form > table > tbody > tr:nth-child(2) > td:nth-child(8) > a")).click();
-		// Cliquer sur la séction "Birds" 
+		// Cliquer sur la sÃ©ction "Birds" 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(9) > img")).click();
 		// Cliquer sur la section "Dogs" 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(3) > img")).click();		
-		// Sélectionner le dernier produit  
+		// SÃ©lectionner le dernier produit  
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(3) > img")).click();
 		// Cliquer sur le premier ID item 
 		driver.findElement(By.cssSelector("#QuickLinks > a:nth-child(3) > img")).click();
@@ -92,7 +92,7 @@ public  class testJacoco {
 		driver.findElement(By.name("username")).sendKeys("Test2");
 		// Renseigner le champ "mot de passe"
 		driver.findElement(By.cssSelector("#Catalog > form > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=\"text\"]")).sendKeys("Test2");
-		// Renseigner le champ "saisir à nouveau le mot de passe 
+		// Renseigner le champ "saisir Ã  nouveau le mot de passe 
 		driver.findElement(By.cssSelector("#Catalog > form > table:nth-child(2) > tbody > tr:nth-child(3) > td:nth-child(2) > input[type=\"text\"]")).sendKeys("Test2");	
 		// Renseigner le champ "FirstName"
 		driver.findElement(By.cssSelector("#Catalog > form > table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(2) > input[type=\"text\"]")).sendKeys("Toto");
